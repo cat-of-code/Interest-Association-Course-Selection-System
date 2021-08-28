138fa972486c93f6e2ebdc29534c5a013a24957d
Ashley112233 <zhangpei93@outlook.com>
2021-08-28 15:41:54 +0800

    修改myActivities页面
    文件修改
      1.新增加俩个控件van-tag和van-toast在app.json中
      2.修改myActivities页面内容
    功能实现
      1.完成退选功能
      2.完成未登录时自动跳转登陆页面
      3.完成对activityDetail页面的跳转，传入参数course_id
    功能特性
      1.展示简要的活动信息，包括活动名称，活动指导老师，活动时间
      2.判断并且显示活动是否过期
      3.右滑活动模块进入操作页面

--- miniprogram/pages/myActivities/myActivities.js
+++ miniprogram/pages/myActivities/myActivities.js
@@ -1,8 +1,11 @@
-// pages/select/select.js
+// pages/myCourse/myCourse.js
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
+
 var app = getApp()
 var db = wx.cloud.database()
 var courseCollection = db.collection("test_db_course")
-var courseListCollection = db.collection("list")
+var userCollection = db.collection("test_db_user")
+var selectListCollection = db.collection("test_db_selectList")
 var _ = db.command
 
 Page({
@@ -11,78 +14,181 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activities: [
-      {
-        "_id": "8937eaa96124ea2e0619dd2b0257e259",
-        "course_name": "篮球基础入门(上)",
-        "course_start_time": "14:00",
-        "course_end_time": "16:00",
-        "course_date": "2021/08/30",
-        "address": "篮球场01",
-        "dec": "简介",
-        "img": "cloud://cloud1-0gyu6anlffcd11a5.636c-cloud1-0gyu6anlffcd11a5-1306965577/basketball.jpeg",
-        "max_people": 10.0,
-        "association_uid": 1.0
-      }, {
-        "_id": "14139e126124eb9006b8cfaf3d517c2e",
-        "course_name": "跑步基础入门(上)",
-        "course_start_time": "14:00",
-        "course_end_time": "16:00",
-        "course_date": "2021/08/30",
-        "address": "田径场01",
-        "dec": "简介",
-        "img": "cloud://cloud1-0gyu6anlffcd11a5.636c-cloud1-0gyu6anlffcd11a5-1306965577/basketball.jpeg",
-        "max_people": 13.0,
-        "association_uid": 2.0
-      }, {
-        "_id": "8937eaa96124ede8061a5eb10279a765",
-        "course_name": "瑜伽基础入门(上)",
-        "course_start_time": "14:00",
-        "course_end_time": "16:00",
-        "course_date": "2021/08/31",
-        "address": "瑜伽室01",
-        "dec": "简介",
-        "img": "cloud://cloud1-0gyu6anlffcd11a5.636c-cloud1-0gyu6anlffcd11a5-1306965577/yoga.jpeg",
-        "max_people": 13.0,
-        "association_uid": 3.0
-      }, {
-        "_id": "8937eaa96124edeb061a5f0e1a7211bf",
-        "course_name": "乒乓球基础入门(上)",
-        "course_start_time": "14:00",
-        "course_end_time": "16:00",
-        "course_date": "2021/08/31",
-        "address": "乒乓球场01",
-        "dec": "简介",
-        "img": "cloud://cloud1-0gyu6anlffcd11a5.636c-cloud1-0gyu6anlffcd11a5-1306965577/basketball.jpeg",
-        "max_people": 20.0,
-        "association_uid": 4.0
-      }, {
-        "_id": "8937eaa96124eded061a5f5a601cd3a9",
-        "course_name": "足球基础入门(上)",
-        "course_start_time": "14:00",
-        "course_end_time": "16:00",
-        "course_date": "2021/09/01",
-        "address": "足球场01",
-        "dec": "简介",
-        "img": "cloud://cloud1-0gyu6anlffcd11a5.636c-cloud1-0gyu6anlffcd11a5-1306965577/basketball.jpeg",
-        "max_people": 13.0,
-        "association_uid": 5.0
-      }, {
-        "_id": "14139e126124edf006b936b773c1b3c6",
-        "course_name": "网球基础入门(上)",
-        "course_start_time": "14:00",
-        "course_end_time": "16:00",
-        "course_date": "2021/09/01",
-        "address": "网球场01",
-        "dec": "简介",
-        "img": "cloud://cloud1-0gyu6anlffcd11a5.636c-cloud1-0gyu6anlffcd11a5-1306965577/basketball.jpeg",
-        "max_people": 22.0,
-        "association_uid": 6.0
-      }
-
-    ],
+    courses: [],
+    userInfo: {},
+    current_time: "",
+  },
+  /**
+   * 退选课程
+   */
+  quit_course(e) {
+    var course_id = e.currentTarget.dataset.course_id
+    var user_id = e.currentTarget.dataset.user_id
+    console.log(course_id)
+    console.log(user_id)
+    if (user_id == this.data.userInfo.openid) {
+      
+      wx.cloud.callFunction({
+        name: 'quitCourse',
+        data: {
+          user_id: user_id,
+          course_id: course_id
+        },
+        success: res => {
+          console.log('[云函数] [quitCourse] '+ res);
+          Toast.success('退选成功');
+          this.onLoad();
+        },
+        fail: err => {
+          console.error('[云函数] [quitCourse]', err);
+          Toast.fail('退选失败');
+        }
+      })
+    }
+  },
+  /**
+   * 跳转到课程详细页面
+   */
+  into_coursePage: function (e) {
+    console.log(e)
+    // app.globalData.courseName = e.currentTarget.dataset.course_id
+    var jf_course_id = JSON.stringify(e.currentTarget.dataset.course_id)
+    wx.navigateTo({
+      url: '../activityDetail/activityDetail?course_id=' + jf_course_id,
+    })
   },
 
+  async getMyCourses(user_openid) {
+    var courseInfos = []
+    var p = await new Promise((resolve, reject) => {
+      selectListCollection.where({
+        _openid: user_openid,
+      }).get().then(res => {
+        courseInfos = res.data
+        resolve(courseInfos)
+      })
+    })
+    console.log(courseInfos)
+    return courseInfos
+  },
 
+  addMyCourses: function (course_ids) {
+    var prom = []
+    var cf = {}
+    var ef = {}
+    var timestamp = Date.parse(new Date());
+    timestamp = timestamp / 1000;
+    //获取当前时间
+    var n = timestamp * 1000;
+    var date = new Date(n);
+    //年
+    var Y = date.getFullYear();
+    //月
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+    //日
+    var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    //时
+    var h = date.getHours();
+    //分
+    var m = date.getMinutes();
+    //秒
+    var s = date.getSeconds();
+    console.log(Y + M + D + h + m)
+
+    for (var i = 0; i < course_ids.length; i++) {
+      console.log(course_ids[i])
+      cf[course_ids[i].course_id] = course_ids[i].enroll_flag
+      var p = new Promise((resolve, reject) => {
+        wx.cloud.callFunction({
+            // 云函数名称
+            name: 'getCourseBInfos',
+            // 传给云函数的参数
+            data: {
+              course_id: course_ids[i].course_id
+            },
+          })
+          .then(res => {
+            var new_res = res.result.list[0]
+            // console.log(course_ids[i].course_id)
+            // course_ids[i]['enroll_flag'] = true
+            console.log(res)
+            resolve(new_res)
+          })
+      })
+      console.log(p)
+      prom.push(p)
+    }
+    Promise.all(prom).then(res => {
+      console.log(res)
+      this.setData({
+        courses: res
+      })
+    })
+    this.setData({
+      courses_flag: cf,
+      current_time: Y + M + D + h + m
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  async onLoad(options) {
+    if (app.globalData.login) {
+      var nickName = app.globalData.nickName
+      var user_openid = app.globalData.openid
+      console.log(nickName)
+      console.log(user_openid)
+      console.log(app.globalData.avatarUrl)
+      var p1 = await this.getMyCourses(user_openid)
+      await this.addMyCourses(p1)
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log(app.globalData.login)
+    if (app.globalData.login) {
+      this.setData({
+        userInfo: {
+          nickName: app.globalData.nickName,
+          avatarUrl: app.globalData.avatarUrl,
+          openid: app.globalData.openid
+        }
+      })
+    } else {
+      wx.switchTab({
+        url: '../user/user',
+      })
+    }
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  async refresh() {
+    var nickName = app.globalData.nickName
+    var user_openid = app.globalData.openid
+    console.log(nickName)
+    console.log(user_openid)
+    console.log(app.globalData.avatarUrl)
+    var p1 = await this.getMyCourses(user_openid)
+    await this.addMyCourses(p1)
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
 
 })
